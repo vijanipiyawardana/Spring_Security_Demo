3f51e225ae689c147f8d5c275ae9936cf23e4509
vijanipiyawardana <vijanipiyawardana@gmail.com>
2024-01-15 23:26:48 +0100

    extract all claims method is complete

--- springsecuritydemo/src/main/java/com/example/springsecuritydemo/service/JwtService.java
+++ springsecuritydemo/src/main/java/com/example/springsecuritydemo/service/JwtService.java
@@ -2,24 +2,31 @@ package com.example.springsecuritydemo.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "f85IhI7qFykaFKkVm2Pbq61YAo+RVnn6EqKEIPOxXABfl73nVOpQPo5UhrDYuFlv";
 
     public String extractUserEmail(String token) {
         return null;
     }
 
     private Claims extractAllClaims(String token) {
-//        return Jwts.parser()
-//                .setSigningKey("")
-//                .parseClaimsJws(token)
-//                .getBody();
-        return Jwts.parserBuilder()
-                .setSigningKey(getSignInKey())
-                .build().parseClaimsJws(token)
-                .getBody();
+        return Jwts.parser()
+                .decryptWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKeySpec getSignInKey() {
+        SignatureAlgorithm sa = SignatureAlgorithm.HS256;
+        return new SecretKeySpec(SECRET_KEY.getBytes(), sa.getJcaName());
     }
 
 }
