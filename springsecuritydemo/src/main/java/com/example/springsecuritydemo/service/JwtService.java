5630f039d2c1f15231e1fd5fb308f0f10aa32b77
vijanipiyawardana <vijanipiyawardana@gmail.com>
2024-01-15 23:59:14 +0100

    added method to generateToken with userDetails and extraClaims

--- springsecuritydemo/src/main/java/com/example/springsecuritydemo/service/JwtService.java
+++ springsecuritydemo/src/main/java/com/example/springsecuritydemo/service/JwtService.java
@@ -3,15 +3,31 @@ package com.example.springsecuritydemo.service;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.spec.SecretKeySpec;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
 
     private static final String SECRET_KEY = "f85IhI7qFykaFKkVm2Pbq61YAo+RVnn6EqKEIPOxXABfl73nVOpQPo5UhrDYuFlv";
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 
     public String extractUserEmail(String token) {
         return extractClaim(token, Claims::getSubject);
