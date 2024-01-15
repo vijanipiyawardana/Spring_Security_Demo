6311f4e1f1ae1edfb5aa05b11163717430c38be8
vijanipiyawardana <vijanipiyawardana@gmail.com>
2024-01-15 17:37:55 +0100

    created JwtAuthenticationFilter

---
+++ springsecuritydemo/src/main/java/com/example/springsecuritydemo/config/JwtAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package com.example.springsecuritydemo.config;
+
+import jakarta.annotation.Nullable;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+    }
+
+}
