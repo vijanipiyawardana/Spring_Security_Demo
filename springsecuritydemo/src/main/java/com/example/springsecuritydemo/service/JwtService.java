e8dd6e60f89b66b8c55e7b7f689bd79748fdb1c2
vijanipiyawardana <vijanipiyawardana@gmail.com>
2024-01-15 21:24:47 +0100

    extract claims from JWT partially done

--- springsecuritydemo/src/main/java/com/example/springsecuritydemo/service/JwtService.java
+++ springsecuritydemo/src/main/java/com/example/springsecuritydemo/service/JwtService.java
@@ -1,5 +1,7 @@
 package com.example.springsecuritydemo.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,6 +9,17 @@ public class JwtService {
 
     public String extractUserEmail(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+//        return Jwts.parser()
+//                .setSigningKey("")
+//                .parseClaimsJws(token)
+//                .getBody();
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build().parseClaimsJws(token)
+                .getBody();
     }
 
 }
