72a16f84fec75d3f8502b1359884ee404129f154
vijanipiyawardana <vijanipiyawardana@gmail.com>
2024-01-16 00:16:29 +0100

    added methods to check token is valid

--- springsecuritydemo/src/main/java/com/example/springsecuritydemo/service/JwtService.java
+++ springsecuritydemo/src/main/java/com/example/springsecuritydemo/service/JwtService.java
@@ -34,13 +34,26 @@ public class JwtService {
                 .compact();
     }
 
-    public String extractUserEmail(String token) {
+    public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     private Claims extractAllClaims(String token) {
