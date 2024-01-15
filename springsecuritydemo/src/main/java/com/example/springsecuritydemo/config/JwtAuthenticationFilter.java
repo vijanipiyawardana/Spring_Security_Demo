02446548569df77e39823020ab7ef8f1b0c0e6c4
vijanipiyawardana <vijanipiyawardana@gmail.com>
2024-01-15 17:40:50 +0100

    added annotations

--- springsecuritydemo/src/main/java/com/example/springsecuritydemo/config/JwtAuthenticationFilter.java
+++ springsecuritydemo/src/main/java/com/example/springsecuritydemo/config/JwtAuthenticationFilter.java
@@ -5,11 +5,15 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
