53fab2a9d719d8d909cdcc90f346cca68777f8dd
vijanipiyawardana <vijanipiyawardana@gmail.com>
2024-01-16 03:33:52 +0100

    implemented register method in AuthenticationService

--- springsecuritydemo/src/main/java/com/example/springsecuritydemo/auth/AuthenticationService.java
+++ springsecuritydemo/src/main/java/com/example/springsecuritydemo/auth/AuthenticationService.java
@@ -1,13 +1,38 @@
 package com.example.springsecuritydemo.auth;
 
+import com.example.springsecuritydemo.entity.User;
+import com.example.springsecuritydemo.enums.Role;
+import com.example.springsecuritydemo.repository.UserRepository;
+import com.example.springsecuritydemo.service.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        repository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+        
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
