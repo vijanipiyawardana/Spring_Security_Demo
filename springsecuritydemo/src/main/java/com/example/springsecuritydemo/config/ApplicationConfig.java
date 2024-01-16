5a6e7ab69b0dc41d6e783cdb8aaee0b2601769c5
vijanipiyawardana <vijanipiyawardana@gmail.com>
2024-01-16 21:33:06 +0100

    avoid making two objects, injected it for the second time

--- springsecuritydemo/src/main/java/com/example/springsecuritydemo/config/ApplicationConfig.java
+++ springsecuritydemo/src/main/java/com/example/springsecuritydemo/config/ApplicationConfig.java
@@ -26,9 +26,9 @@ public class ApplicationConfig {
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider() {
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
